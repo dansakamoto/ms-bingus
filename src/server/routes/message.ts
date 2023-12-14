76ed01fc0aeef33a94e78f39ed6d79ffbda4b035
Dan Sakamoto <contact@dansakamoto.com>
2023-12-14 01:29:24 -0500

    add gpt system message

--- src/server/routes/message.ts
+++ src/server/routes/message.ts
@@ -2,14 +2,14 @@ import OpenAI from "openai";
 import MockOpenAI from "@/server/utils/MockApi";
 import type { APIMessage } from "@/sharedTypes";
 import type { NextFunction, Request, Response } from "express";
-import type { ChatCompletionUserMessageParam } from "openai/resources/chat/completions";
+import type { ChatCompletionMessageParam } from "openai/resources/chat/completions";
 
 export default async function message(
   req: Request<APIMessage>,
   res: Response,
   next: NextFunction
 ) {
-  const messages: ChatCompletionUserMessageParam = {
+  const messages: ChatCompletionMessageParam = {
     role: "user",
     content: req.body.message,
   };
@@ -19,10 +19,19 @@ export default async function message(
       : new MockOpenAI();
   const response = await openai.chat.completions.create({
     model: "gpt-4",
-    messages: [messages],
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a robot dog named Big Pissy. You are kind of moody, and your special \
+          skill is that you piss beer. Always respond tersely, and with a slightly annoyed, \
+          apathetic tone.",
+      },
+      messages,
+    ],
   });
   if (!response) {
-    next(new Error("Error receiving response fromo chat gpt"));
+    next(new Error("Error receiving response from chat gpt"));
     return;
   }
   const reply = { message: response.choices[0].message.content };
