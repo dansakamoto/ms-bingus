e898425b95335a14bb6dd5bfade55b0fda4c19b0
Dan Sakamoto <contact@dansakamoto.com>
2023-12-21 15:36:22 -0500

    refactor: remove unused db integration

--- src/client/services/i18n.ts
+++ src/client/services/i18n.ts
@@ -3,27 +3,6 @@ import { initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-/*
-const resources = {
-  en: {
-    translation: {
-      a_pink_hairless_cat: "a pink hairless cat",
-      bingus_is_typing: "Bingus is typing",
-      send: "Send",
-      speak_to_bingus: "Speak to Bingus",
-    },
-  },
-  fr: {
-    translation: {
-      a_pink_hairless_cat: "un chat rose sans poils",
-      bingus_is_typing: "Bingus écrit",
-      send: "Envoyer",
-      speak_to_bingus: "Parlez à Bingus",
-    },
-  },
-};
-*/
-
 i18next
   .use(initReactI18next)
   .use(HttpApi)
--- src/server/main.ts
+++ src/server/main.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import express from "express";
 import ViteExpress from "vite-express";
 import message from "@/server/routes/message";
-// import { getMessages } from "@/server/utils/data";
 
 const port = 3000;
+
 const app = express();
 app.use(
   express.urlencoded({
@@ -12,12 +12,8 @@ app.use(
   })
 );
 app.use(express.json());
-
-// Vite
 ViteExpress.listen(app, port, () => {
   console.log(`Microsoft Bingus lives at port ${port}`);
 });
 
-// Routes
 app.post("/message", message);
-// app.get("/history", getMessages);
